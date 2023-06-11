eec01d5fa32a2d8ced777e232f6cec297bea94c9
NakibDiu <nakib15-3358@diu.edu.bd>
2023-06-12 02:15:25 +0600

    firebase intialized

--- src/main.jsx
+++ src/main.jsx
@@ -3,19 +3,19 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/Routes.jsx";
-import {
-  QueryClient,
-  QueryClientProvider,
-} from "react-query";
+import { QueryClient, QueryClientProvider } from "react-query";
+import AuthProvider from "./providers/AuthProvider";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <div className="max-w-[1440px] 2xl:mx-auto mx-6 lg:mx-10">
-      <QueryClientProvider client={queryClient}>
-        <RouterProvider router={router} />
-      </QueryClientProvider>
+      <AuthProvider>
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
+      </AuthProvider>
     </div>
   </React.StrictMode>
 );
